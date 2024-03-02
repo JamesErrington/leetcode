fbefd572ca6752f76f109963ba554085dbfa3cec
JamesErrington <james.errington@protonmail.com>
2024-03-02 15:15:36 +0000

    Coderbyte: Bracket Combinations

--- coderbyte/hard/bracket_combinations.py
+++ coderbyte/hard/bracket_combinations.py
@@ -7,24 +7,32 @@ and return the number of valid combinations that can be formed with num pairs of
 For example, if the input is 3, then the possible combinations of 3 pairs of parenthesis, namely: ()()(), are ()()(), ()(()), (())(), ((())), and (()()).
 There are 5 total combinations when the input is 3, so your program should return 5.
 '''
+count = 0
 
 def BracketCombinations(num: int) -> int:
-    str = "()" * num
-    print(str)
-    return 1
+    if num == 0:
+        return 1
+    global count
+    count = 0
+    branch(num * 2, 0, 0, "")
+    return count
 
-# 5 42
-# 4 14
-# print(BracketCombinations(3) == 5) # ()()(), ()(()), (())(), ((())), (()())
-# print(BracketCombinations(2) == 2) # ()(), (())
-# print(BracketCombinations(1) == 1) # ()
-# print(BracketCombinations(0) == 0)
+def branch(n: int, o: int, c: int, s: str):
+    if n == 0:
+        if o == c:
+            global count
+            count += 1
+        return
+    if len(s) == 0:
+        branch(n - 1, o + 1, c, s+"(")
+    else:
+        branch(n - 1, o + 1, c, s+"(")
+        if o > c:
+            branch(n - 1, o, c + 1, s+")")
 
-
-'''
-1: 1
-2: 1 + 1
-3: 1 + 2 + 2
-4: 1 + 2 + 5 + 6
-5: 1 + 2 + 5 + 14 + 20
-'''
+print(BracketCombinations(5) == 42)
+print(BracketCombinations(4) == 14)
+print(BracketCombinations(3) == 5)
+print(BracketCombinations(2) == 2)
+print(BracketCombinations(1) == 1)
+print(BracketCombinations(0) == 0)
